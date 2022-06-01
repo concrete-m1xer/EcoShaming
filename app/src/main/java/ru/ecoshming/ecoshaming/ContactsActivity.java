7aad1158dacaa90f0987f9931735946525b453ae
vslav tkacev <slava.tkachyov@mail.ru>
2022-06-01 06:29:45 +0300

    add 1 more activity, finish login/registration

--- app/src/main/java/ru/ecoshming/ecoshaming/ContactsActivity.java
+++ app/src/main/java/ru/ecoshming/ecoshaming/ContactsActivity.java
@@ -12,7 +12,9 @@ public class ContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
-        this.getActionBar().hide();
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         ImageButton imgbtn3 = (ImageButton) findViewById(R.id.imageButton3);
         imgbtn3.setOnClickListener(new View.OnClickListener() {
