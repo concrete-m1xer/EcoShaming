8ce5de2eb226cb0f887b6f819b3cc9da198a7d1e
vslav tkacev <slava.tkachyov@mail.ru>
2022-06-01 09:46:13 +0300

    merge branches

--- app/src/main/java/ru/ecoshming/ecoshaming/Colomenskoe.java
+++ app/src/main/java/ru/ecoshming/ecoshaming/Colomenskoe.java
@@ -1,14 +1,18 @@
 package ru.ecoshming.ecoshaming;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Colomenskoe extends AppCompatActivity implements View.OnClickListener {
 
@@ -53,6 +57,13 @@ public class Colomenskoe extends AppCompatActivity implements View.OnClickListen
     public void SetImageText(String imageName, String text){
         setContentView(R.layout.colomenskoe_set);
         ImageView img = (ImageView) findViewById(R.id.app_bar_image);
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(imageName);
+
+        Glide.with(this)
+                .load(storageReference)
+                .into(img);
+        TextView tv = (TextView) findViewById(R.id.text_discribtion);
+        tv.setText(text);
     }
 
     @Override
@@ -60,11 +71,11 @@ public class Colomenskoe extends AppCompatActivity implements View.OnClickListen
         int i = v.getId();
         switch (i){
             case (R.id.back_to_route_descr): {
-
+                setContentView(R.layout.activity_colomenskoe);
                 break;
             }
             case (R.id.shore_discr): {
-
+                SetImageText("Коломенская Набережная.jpg", getString(R.string.colomensky_shore));
                 break;
             }
             case (R.id.gorodishe_discr): {
